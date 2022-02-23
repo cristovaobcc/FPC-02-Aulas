1b2d26086571f51e43356c1a6d4093727634629b
cristovaobcc <cristovao.bcc@gmail.com>
2022-02-23 20:54:23 -0300

    Testando chamada de equals em subclasse de Object
    Relembrando se há chamada de equals de uma classe q
    implementa o equals. Deu certo. Há chamada de equals
    da subclasse de Object, Integer no exemplo abaixo.
    Se o dado passado implementar o equals seu método será
    chamado no equals da classe Dado.

--- src/aula01/arvore/Dado.java
+++ src/aula01/arvore/Dado.java
@@ -24,7 +24,58 @@ public class Dado {
 	public void setDado(Object dado) {
 		this.dado = dado;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dado == null) ? 0 : dado.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dado other = (Dado) obj;
+		if (dado == null) {
+			if (other.dado != null)
+				return false;
+		} else if (!dado.equals(other.dado))
+			return false;
+		return true;
+	}
 	
+	@Override
+	public String toString() {
+		return this.dado.toString();
+	}
 	
+	public static void main(String[] args) {
+		// Relembrando se há chamada de equals de uma classe q
+		// implementa o equals. Deu certo. Há chamada de equals
+		// da subclasse de Object, Integer no exemplo abaixo.
+		// Se o dado passado implementar o equals seu método será
+		// chamado no equals da classe Dado.
+		
+		Dado dado1 = new Dado(Integer.valueOf(4));
+		Dado dado2 = new Dado(Integer.valueOf(5));
+		Dado dado3 = new Dado(Integer.valueOf(4));
+		Integer num4 = Integer.valueOf(4);
+		System.out.println(num4.equals(dado1.dado) ? "true" : "false");
+		if(dado1.equals(dado3)) {
+			System.out.println(dado1.toString());
+		} else {
+			System.out.println("Chamou o equals da superclasse.");
+		}
+		if (dado2.equals(dado3)) {
+			System.out.println("Não é p/ entrar aqui");
+		}
+		System.out.println("Hello world.");
+	}
 	
 }
