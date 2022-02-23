028be4fe1264ecb2ac95e8435bbda5b924264452
cristovaobcc <cristovao.bcc@gmail.com>
2022-02-23 16:44:26 -0300

    Implementação de um nodo de árvore binária
    Por ora a implementação contempla apenas elementos básicos: dados, nó
    pai, nó esquerdo e nó direito.

---
+++ src/aula01/arvore/Dado.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package aula01.arvore;
+
+/**
+ * Classe que representa os dados de um nodo de árvore binária
+ * @author cristovao
+ *
+ */
+public class Dado {
+
+	private Object dado;
+
+	public Dado(Object dado) {
+		super();
+		this.dado = dado;
+	}
+
+	public Object getDado() {
+		return dado;
+	}
+
+	public void setDado(Object dado) {
+		this.dado = dado;
+	}
+	
+	
+	
+}
---
+++ src/aula01/arvore/Nodo.java
@@ -0,0 +1,67 @@
+package aula01.arvore;
+
+/**
+ * Classe que representa o nó de uma árvore binária.
+ * 
+ * @author cristovao
+ *
+ */
+public class Nodo {
+
+	private Dado dados;
+	private Nodo pai;
+	private Nodo nodoEsq;
+	private Nodo nodoDir;
+	
+	/**
+	 * Ao instanciar um nodo deve-se passar um conteúdo que seja do tipo Dado.
+	 * Caso nodo raiz seja null, o nó
+	 * 
+	 * @param dados @Dado
+	 * @param pai @Nodo
+	 * @param nodoEsq @Nodo
+	 * @param nodoDir @Nodo
+	 */
+	public Nodo(Dado dados, Nodo pai, Nodo nodoEsq, Nodo nodoDir) {
+		super();
+		this.dados = dados;
+		this.pai = pai;
+		this.nodoEsq = nodoEsq;
+		this.nodoDir = nodoDir;
+	}
+
+	public Dado getDados() {
+		return dados;
+	}
+
+	public void setDados(Dado dados) {
+		this.dados = dados;
+	}
+
+	public Nodo getPai() {
+		return pai;
+	}
+
+	public void setPai(Nodo raiz) {
+		this.pai = raiz;
+	}
+
+	public Nodo getNodoEsq() {
+		return nodoEsq;
+	}
+
+	public void setNodoEsq(Nodo nodoEsq) {
+		this.nodoEsq = nodoEsq;
+	}
+
+	public Nodo getNodoDir() {
+		return nodoDir;
+	}
+
+	public void setNodoDir(Nodo nodoDir) {
+		this.nodoDir = nodoDir;
+	}
+	
+	
+
+}
