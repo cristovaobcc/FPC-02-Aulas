6b9334b8628d226ea4d15292b675215ccbdaba7f
cristovaobcc <cristovao.bcc@gmail.com>
2022-03-02 15:33:47 -0300

    Cria classe (em implementação) ArvoreBinaria

---
+++ src/aula01/arvore/ArvoreBinaria.java
@@ -0,0 +1,23 @@
+package aula01.arvore;
+
+public class ArvoreBinaria {
+	
+	private Nodo raiz;
+	
+	public ArvoreBinaria(Nodo raiz) {
+		this.raiz = raiz;
+	}
+
+	public Nodo getRaiz() {
+		return raiz;
+	}
+
+	public void setRaiz(Nodo raiz) {
+		this.raiz = raiz;
+	}
+	
+	public void insereNodo(Dado dado) {
+		// TODO: implementar a inserção em 1 das 3 ordens.
+	}
+	
+}
