caf837ba89c9d881e6b3ad2f4a31391f86d6bbb0
cristovaobcc <cristovao.bcc@gmail.com>
2022-03-03 14:21:28 -0300

    Implementação de funções listarEmOrdemRecursivo() e percorreEmOrdem()
    Ambas as funções foram implementadas na classe ArvoreBinaria.

--- src/aula01/arvore/ArvoreBinaria.java
+++ src/aula01/arvore/ArvoreBinaria.java
@@ -1,13 +1,22 @@
 package aula01.arvore;
 
+import java.util.ArrayList;
+
 public class ArvoreBinaria {
 	
 	private Nodo raiz;
 	
-	public ArvoreBinaria(Nodo raiz) {
-		this.raiz = raiz;
+	public ArvoreBinaria() { }
+	
+	/**
+	 * Instancia uma nova árvore com o objeto raiz como 
+	 * raiz da árvore.
+	 * @param raiz Object
+	 */
+	public ArvoreBinaria(Object raiz) {
+		this.raiz = new Nodo(raiz, null);
 	}
-
+	
 	public Nodo getRaiz() {
 		return raiz;
 	}
@@ -16,8 +25,65 @@ public class ArvoreBinaria {
 		this.raiz = raiz;
 	}
 	
-	public void insereNodo(Dado dado) {
+	/**
+	 * Insere o dado na árvore binária.
+	 * 
+	 * @param dado Object
+	 */
+	public void insereNodo(Object dado) {
 		// TODO: implementar a inserção em 1 das 3 ordens.
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<Object> listarEmOrdemRecursivo(){
+		
+		ArrayList<Object> arrayList = new ArrayList<Object>();
+		arrayList.add(this.raiz);
+		
+		if (this.raiz.getNodoEsq() != null ) {
+			this.percorreEmOrdem(this.raiz.getNodoEsq(), arrayList);
+		} 
+		
+		if (this.raiz.getNodoDir() != null) {
+			this.percorreEmOrdem(this.raiz.getNodoDir(), arrayList);
+		}
+		
+		return arrayList;
+	}
+	
+	
+	/**
+	 * Percorre em ordem a árvore e add o nodo n na lista.
+	 * @param n Nodo
+	 * @param lista {@link ArrayList}
+	 */
+	private void percorreEmOrdem(Nodo n, ArrayList<Object> lista) {
+		// Verifica se o nó atual tem filho esquerdo.
+		if (n.getNodoEsq() != null) {
+			this.percorreEmOrdem(n.getNodoEsq(), lista);
+		} 
+		lista.add(n);	
+		// Verifica se o nó atual não tem filho direito.
+		if (n.getNodoDir() != null) {
+			this.percorreEmOrdem(n.getNodoDir(), lista);
+		}		
+	}
+	
+	/**
+	 * 
+	 * @param nodo
+	 * @return
+	 */
+	private Nodo visitaSubArvore(Nodo nodo) {
+		if (nodo.getNodoEsq() == null) {
+			return nodo;
+		} else {
+			return this.visitaSubArvore(nodo.getNodoDir());
+		}
+		
 	}
 	
 }
