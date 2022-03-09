7eb2aeca97244b26df1d754775e17ef0824b6149
cristovaobcc <cristovao.bcc@gmail.com>
2022-03-09 16:37:42 -0300

    Testa método ArvoreBinaria.percorreEmOrdem()
    O teste é feito primeiro c/ um array de controle (numeros = {21, 30, 31,
    25, 27, 22, 8, 6, 7}) e o resultado é observado na saída.
    O resultado obtido foi 21, 8, 6, 7, 30, 25, 22, 27, 31.

--- src/aula01/testes/AppDeTestes.java
+++ src/aula01/testes/AppDeTestes.java
@@ -22,34 +22,36 @@ public class AppDeTestes {
 	 */
 	public static void main(String[] args) {
 		imprimeLinha("Lista de números inteiros aleatórios para árvore");
-		
+
 		List<Integer> elementosParaArvore = new LinkedList<Integer>();
+
+		// Random random = new Random();	
+		// elementosParaArvore = insereInteirosAleatorios(elementosParaArvore);
 		
-		Random random = new Random();
+		Integer[] numeros = {21, 30, 31, 25, 27, 22, 8, 6, 7};
 		
-		for (int i = 0, j = 0; i < 10; i++) {
-			j = random.nextInt(100);
-			elementosParaArvore.add(j);
-		}
+		elementosParaArvore = insereNumerosDeArray(numeros, elementosParaArvore);
+		
 		
 		System.out.println(elementosParaArvore);
-		
+
 		ArvoreBinaria arvoreBinaria = new ArvoreBinaria();
-		
-		Integer valor = random.nextInt(100);
-		Nodo n = new Nodo(valor, null);
+
+		// Integer valor = random.nextInt(100);
+		// Nodo n = new Nodo(valor, null);
+		Nodo n = new Nodo(numeros[0], null);
 		imprimeLinha("Valor inicial da raiz");
 		System.out.println(n);
 		arvoreBinaria.setRaiz(n);
 		imprimeLinha("Valor inicial de árvore binária");
 		System.out.println(arvoreBinaria);
-		
+
 		for (Integer integer : elementosParaArvore) {
 			arvoreBinaria.insereNodo(integer);
 		}
-		
+
 		List<Object> elementosPercorridosEmOrdem = arvoreBinaria.listarEmOrdemRecursivo();
-		
+
 		imprimeLinha("Árvore percorrida em ordem");
 		int tamanho = elementosPercorridosEmOrdem.size();
 		Nodo nodoLido = null;
@@ -61,13 +63,29 @@ public class AppDeTestes {
 				System.out.printf(nodoLido.getDados().toString() + ", ");
 			}
 		}
-		
-		
-		
-		
 
 	}
-	
+
+	private static List<Integer> insereNumerosDeArray(Integer[] numeros, List<Integer> elementosParaArvore) {
+		for (int i = 0; i < numeros.length; i++) {
+			Integer integer = numeros[i];
+			elementosParaArvore.add(integer);
+		}
+		return elementosParaArvore;
+	}
+
+
+	public static List<Integer> insereInteirosAleatorios(List<Integer> list) {
+		Random random = new Random();
+
+		for (int i = 0, j = 0; i < 10; i++) {
+			j = random.nextInt(100);
+			list.add(j);
+		}
+		
+		return list;
+	}
+
 	/**
 	 * Imprime linha com 50 "-" e um texto no meio delas;
 	 * @param texto String
